4ad83dca2b6d48b5ba86a57225b04facd74ab67a
Kevin Lopez Quiroga <kevin.lopez@bg.com.bo>
2024-12-13 20:30:55 +0000

    Merged PR 17449: [BFF] Modificación de la estructura de mensajes de error para ThirdAccountManager
    [NGM-8953](https://acelerabga.atlassian.net/browse/NGM-8953) Modificación de la estructura de mensajes de error para ThirdAccountManager
    
    Componentes:
    * ThirdAccountMiddlewareError
    
    Cherry picked from !17438
    
    Cherry-picked from commit `03265cba`.
    
    Cherry picked from !17448
    
    Cherry-picked from commit `c3bff87a`.

--- bff/src/main/java/bg/com/bo/bff/providers/models/enums/middleware/third/account/ThirdAccountMiddlewareError.java
+++ bff/src/main/java/bg/com/bo/bff/providers/models/enums/middleware/third/account/ThirdAccountMiddlewareError.java
@@ -1,5 +1,6 @@
 package bg.com.bo.bff.providers.models.enums.middleware.third.account;
 
+import bg.com.bo.bff.commons.enums.CategoryError;
 import bg.com.bo.bff.providers.models.interfaces.middleware.IMiddlewareError;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,36 +10,46 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ThirdAccountMiddlewareError implements IMiddlewareError {
 
-    MDWACTM_002(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-002", "No se encontraron cuentas disponibles.", "No hay cuentas disponibles"),
-    MDWACTM_004(HttpStatus.NOT_FOUND, "NOT_ACCOUNT", "MDWACTM-004", "No se encontró la cuenta especificada para eliminar.", "Cuenta no encontrada"),
-    MDWACTM_009(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-009", "Los datos ingresados no existen. Verificalos e intenta nuevamente.", "Destinatario no encontrado"),
-    MDWACTM_015(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "MDWACTM-015", "Los parámetros proporcionados deben ser iguales para el canal 2.", "Error de parámetros"),
-    MDWACTM_016(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "MDWACTM-016", "Los parámetros deben ser distintos para el canal 6.", "Error de parámetros"),
-    MDWACTM_017(HttpStatus.NOT_FOUND, "NOT_ACCOUNT", "MDWACTM-017", "No se encontró la cuenta con los datos proporcionados.", "Cuenta no encontrada"),
-    MDWACTM_0001(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "MDWACTM-0001", "Ocurrió un error al agregar la cuenta. Por favor, revisa los datos e intenta nuevamente.", "Error al agregar cuenta"),
-    MDWRLIB_0003(HttpStatus.NOT_FOUND, "NOT_ACCOUNT", "MDWRLIB-0003", "No se encontró la cuenta solicitada.", "Cuenta no encontrada"),
-    MDWACTM_0006(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-0006", "No se encontró la cuenta especificada. Revisa los datos e intenta nuevamente.", "Cuenta no encontrada"),
-    MDWACTM_0007(HttpStatus.NOT_FOUND, "NOT_ACCOUNT", "MDWACTM-0007", "La cuenta está bloqueada. Comunícate con soporte para más detalles.", "Cuenta bloqueada"),
-    MDWACTM_0008(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-0008", "La cuenta ingresada es incorrecta, no corresponde a una cuenta de terceros.", "Cuenta incorrecta"),
-    MDWACTM_0009(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-0009", "La cuenta especificada ha sido cancelada y no está disponible para operaciones.", "Cuenta cancelada"),
-    MDWACTM_0010(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "MDWACTM-0010", "La cuenta ya se encuentra registrada.", "Cuenta registrada"),
-    MDWACTM_1002(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-1002", "No se encontraron registros asociados a la cuenta.", "Datos inválidos"),
-    MDWACTM_2004(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "MDWACTM-2004", "No se pudo encontrar la cuenta con los datos proporcionados.", "Cuenta no encontrada"),
-    MDWACTM_2007(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-2007", "La cuenta ya se encuentra registrada.", "Cuenta registrada"),
-    MDWACTM_2005(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-2005", "La cuenta especificada ha sido cancelada.", "Cuenta cancelada"),
-    MDWACTM_2006(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-2006", "La cuenta está bloqueada. Contacta a soporte.", "Cuenta bloqueada"),
-    MDWACTM_2001(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-2001", "Ocurrió un error al agregar la cuenta. Intenta nuevamente más tarde.", "Error al agregar cuenta"),
-    MDWACTM_2002(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-2002", "Ocurrió un error al agregar la cuenta. Revisa los datos e intenta nuevamente.", "Error al agregar cuenta"),
-    MDWACTM_2003(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "MDWACTM-2003", "La cuenta especificada es incorrecta. Por favor, verifica los datos proporcionados.", "Cuenta incorrecta"),
-    MDWACTM_2008(HttpStatus.NOT_ACCEPTABLE, "NOT_ACCEPTABLE", "MDWACTM-2008", "Ocurrió un error al agregar la cuenta. Intenta nuevamente.", "Error al agregar cuenta"),
-    MDWACTM_3001(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "MDWACTM-3001", "No se encontró la cuenta solicitada.", "Cuenta no encontrada"),
-    MDWACTM_3002(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-3002", "No se encontró la cuenta con los datos proporcionados.", "Cuenta no encontrada"),
-    MDWACTM_3003(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "MDWACTM-3003", "Ocurrió un error al eliminar la cuenta. Intenta nuevamente.", "Error al eliminar cuenta"),
-    ERROR_ADD_ACCOUNT(HttpStatus.BAD_REQUEST, "BAD_REQUEST", null, "Ocurrió un error al agregar la cuenta. Revisa los datos e intenta nuevamente.", "Error al agregar cuenta");
+    MDWACTM_0001(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-0001", "No se pudo agregar la cuenta. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+    MDWACTM_0003(HttpStatus.NOT_IMPLEMENTED, "INVALID_VERSION", "MDWACTM-0003", "La versión es inválida.", "Versión inválida", CategoryError.NO_HEADER_PARAMS_MW_ERROR.getCategoryId()),
+    MDWACTM_0004(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-0004", "No se pudo agregar la cuenta. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+    MDWACTM_0005(HttpStatus.CONFLICT, "ACCOUNT_REGISTERED", "MDWACTM-0005", "La cuenta ya se encuentra registrada.", "Cuenta registrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_0006(HttpStatus.CONFLICT, "INVALID_ACCOUNT", "MDWACTM-0006", "No se encontró la cuenta especificada. Revisa los datos e intenta nuevamente.", "Cuenta no encontrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_0007(HttpStatus.CONFLICT, "ACCOUNT_BLOCKED", "MDWACTM-0007", "La cuenta está bloqueada. Comunícate con soporte para más detalles.", "Cuenta bloqueada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_0008(HttpStatus.CONFLICT, "INVALID_ACCOUNT", "MDWACTM-0008", "No se encontró la cuenta especificada. Revisa los datos e intenta nuevamente.", "Cuenta no encontrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_0009(HttpStatus.CONFLICT, "ACCOUNT_KO", "MDWACTM-0009", "La cuenta especificada ha sido cancelada y no está disponible para operaciones.", "Cuenta cancelada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_0010(HttpStatus.CONFLICT, "ACCOUNT_REGISTERED", "MDWACTM-0010", "La cuenta ya se encuentra registrada.", "Cuenta registrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_0011(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-0011", "No se pudo agregar la cuenta. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+
+    MDWACTM_1001(HttpStatus.NOT_IMPLEMENTED, "INVALID_VERSION", "MDWACTM-1001", "La versión es inválida.", "Versión inválida", CategoryError.NO_HEADER_PARAMS_MW_ERROR.getCategoryId()),
+    MDWACTM_1002(HttpStatus.NOT_ACCEPTABLE, "REGISTERS_NOT_FOUND", "MDWACTM-1002", "No se encontraron registros relacionados.", "Datos no encontrados", CategoryError.MW_GENERIC_INTERNAL_FAILURE.getCategoryId()),
+
+    MDWACTM_2001(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-2001", "No se pudo agregar la cuenta billetera. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+    MDWACTM_2002(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-2002", "No se pudo agregar la cuenta billetera. Inténtalo nuevamente", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+    MDWACTM_2003(HttpStatus.CONFLICT, "INVALID_WALLET_ACCOUNT", "MDWACTM-2003", "No se encontró la cuenta billetera especificada. Revisa los datos e intenta nuevamente.", "Cuenta no encontrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_2004(HttpStatus.CONFLICT, "INVALID_ACCOUNT", "MDWACTM-2004", "No se encontró la cuenta especificada. Revisa los datos e intenta nuevamente.", "Cuenta no encontrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_2005(HttpStatus.CONFLICT, "ACCOUNT_KO", "MDWACTM-2005", "La cuenta especificada ha sido cancelada y no está disponible para operaciones.", "Cuenta cancelada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_2006(HttpStatus.CONFLICT, "ACCOUNT_KO", "MDWACTM-2006", "La cuenta especificada no permite créditos y no está disponible para operaciones.", "Cuenta inválida", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_2007(HttpStatus.CONFLICT, "ACCOUNT_REGISTERED", "MDWACTM-2007", "La cuenta ya se encuentra registrada.", "Cuenta registrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_2008(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-2008", "No se pudo agregar la cuenta billetera. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+
+    MDWACTM_3001(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-3001", "No se pudo obtener el registro de la cuenta. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+    MDWACTM_3002(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-3002", "Tuvimos un problema interno. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_GENERIC_INTERNAL_FAILURE.getCategoryId()),
+    MDWACTM_3003(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", "MDWACTM-3003", "No se pudo eliminar la cuenta billetera. Inténtalo nuevamente.", "Ocurrió un problema", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+
+    MDWACTM_002(HttpStatus.NOT_ACCEPTABLE, "ACCOUNT_NOT_FOUND", "MDWACTM-002", "No se encontraron cuentas disponibles.", "No hay cuentas disponibles", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId()),
+    MDWACTM_004(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-004", "No se encontró una cuenta relacionada a la solicitud.", "Datos no encontrados", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_009(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-009", "Los datos ingresados no existen. Verificalos e intenta nuevamente.", "Destinatario no encontrado", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+    MDWACTM_015(HttpStatus.NOT_IMPLEMENTED, "INVALID_PARAMS", "MDWACTM-015", "Los parámetros deben ser los mismos para el canal 2.", "Parámetros inválidos", CategoryError.NO_HEADER_PARAMS_MW_ERROR.getCategoryId()),
+    MDWACTM_016(HttpStatus.NOT_IMPLEMENTED, "INVALID_PARAMS", "MDWACTM-016", "Los parámetros deben ser diferentes para el canal 6.", "Parámetros inválidos", CategoryError.NO_HEADER_PARAMS_MW_ERROR.getCategoryId()),
+    MDWACTM_017(HttpStatus.CONFLICT, "INVALID_DATA", "MDWACTM-017", "No se encontró la cuenta con los datos proporcionados.", "Cuenta no encontrada", CategoryError.MW_GENERIC_FAIL_RESPONSE.getCategoryId()),
+
+    ERROR_ADD_ACCOUNT(HttpStatus.NOT_ACCEPTABLE, "ERROR_PROCEDURE", null, "No se pudo agregar la cuenta. Revisa los datos e intenta nuevamente.", "Error al agregar cuenta", CategoryError.MW_SPECIFIC_INTERNAL_FAILURE.getCategoryId());
 
     private final HttpStatus httpCode;
     private final String code;
     private final String codeMiddleware;
     private final String message;
     private final String title;
+    private final int getCategoryId;
 }
