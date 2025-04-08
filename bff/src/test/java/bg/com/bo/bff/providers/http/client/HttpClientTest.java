68167880a2d735150ac9b53ab8651fba7b121564
stephani.heredia <stephani.heredia@bg.com.bo>
2025-04-08 09:43:12 -0400

    Fix sonar cobertura and issues

--- bff/src/test/java/bg/com/bo/bff/providers/http/client/HttpClientTest.java
+++ bff/src/test/java/bg/com/bo/bff/providers/http/client/HttpClientTest.java
@@ -4,6 +4,7 @@ import bg.com.bo.bff.application.exceptions.HandledException;
 import bg.com.bo.bff.commons.interfaces.IHttpClientFactory;
 import bg.com.bo.bff.providers.models.enums.middleware.response.GenericControllerErrorResponse;
 import bg.com.bo.bff.providers.models.external.services.HttpClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
@@ -11,8 +12,10 @@ import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicStatusLine;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
@@ -76,12 +79,16 @@ class HttpClientTest {
 
     @Test
     void handleResponse_shouldThrowUnauthorized() throws Exception {
+        // Arrange
         CloseableHttpResponse httpResponse = mockHttpResponse(HttpStatus.UNAUTHORIZED.value(), null);
         when(httpClientFactory.create()).thenReturn(mockHttpClient);
         when(mockHttpClient.execute(any())).thenReturn(httpResponse);
 
-        assertThrows(HandledException.class, () ->
-                httpClient.executeGetRequest("http://localhost", Map.of(), DummyResponse.class));
+        // Act
+        Executable executable = () -> httpClient.executeGetRequest("http://localhost", Map.of(), DummyResponse.class);
+
+        // Assert
+        Assertions.assertThrows(HandledException.class, executable);
     }
 
     @Test
@@ -90,11 +97,11 @@ class HttpClientTest {
         when(httpClientFactory.create()).thenReturn(mockHttpClient);
         when(mockHttpClient.execute(any())).thenReturn(httpResponse);
 
-        HandledException ex = assertThrows(HandledException.class, () ->
-                httpClient.executeGetRequest("http://localhost", Map.of(), DummyResponse.class));
+        Executable executable = () -> httpClient.executeGetRequest("http://localhost", Map.of(), DummyResponse.class);
+
+        HandledException ex = assertThrows(HandledException.class, executable);
         assertEquals(GenericControllerErrorResponse.NOT_HANDLED_RESPONSE.getCode(), ex.getCode());
     }
-
 
     private CloseableHttpResponse mockHttpResponse(int statusCode, Object responseBody) throws IOException {
         CloseableHttpResponse response = mock(CloseableHttpResponse.class);
@@ -110,6 +117,44 @@ class HttpClientTest {
         }
 
         return response;
+    }
+
+    @Test
+    void executePostRequest_shouldThrowHandledException_whenRequestBuildFails() throws Exception {
+        ObjectMapper failingMapper = mock(ObjectMapper.class);
+        when(failingMapper.writeValueAsString(any()))
+                .thenThrow(new JsonProcessingException("fail") {});
+
+        HttpClient httpClientWithFailingMapper = new HttpClient(httpClientFactory, failingMapper);
+
+        Executable executable = () -> httpClientWithFailingMapper.executePostRequest(
+                "http://localhost", new DummyRequest("fail"), Map.of(), DummyResponse.class
+        );
+
+        HandledException ex = assertThrows(HandledException.class, executable);
+        assertEquals(GenericControllerErrorResponse.REQUEST_EXCEPTION.getCode(), ex.getCode());
+    }
+
+
+    @Test
+    void executeRequest_shouldThrowHandledException_whenHttpClientCreationFails() {
+        when(httpClientFactory.create()).thenThrow(new RuntimeException("Factory fail"));
+
+        Executable executable = () -> httpClient.executeGetRequest("http://localhost", Map.of(), DummyResponse.class);
+
+        HandledException ex = assertThrows(HandledException.class, executable);
+        assertEquals(GenericControllerErrorResponse.HTTP_CLIENT_CREATION_EXCEPTION.getCode(), ex.getCode());
+    }
+
+    @Test
+    void executeAndHandleResponse_shouldThrowHandledException_whenRequestFails() throws Exception {
+        when(httpClientFactory.create()).thenReturn(mockHttpClient);
+        when(mockHttpClient.execute(any())).thenThrow(new RuntimeException("Execution failed"));
+
+        Executable executable = () -> httpClient.executeGetRequest("http://localhost", Map.of(), DummyResponse.class);
+
+        HandledException ex = assertThrows(HandledException.class, executable);
+        assertEquals(GenericControllerErrorResponse.REQUEST_EXCEPTION.getCode(), ex.getCode());
     }
 
     public static class DummyRequest {
