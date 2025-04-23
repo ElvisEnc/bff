289be20e643a0fa13aa15b3bf90cb4d0ced815dc
stephani.heredia <stephani.heredia@bg.com.bo>
2025-04-23 10:10:16 -0400

    adicion de mensajes de error

--- bff/src/main/java/bg/com/bo/bff/providers/dtos/response/loyalty/LoyaltyRegisterSubscriptionResponse.java
+++ bff/src/main/java/bg/com/bo/bff/providers/dtos/response/loyalty/LoyaltyRegisterSubscriptionResponse.java
@@ -1,7 +1,6 @@
 package bg.com.bo.bff.providers.dtos.response.loyalty;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
