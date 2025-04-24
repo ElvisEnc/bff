cc1830830a144fd777069e3cd53a1a8aefb26c8f
nicolas.andrade <jose.andrade@bg.com.bo>
2025-04-24 16:35:34 -0400

    Ãfix sonar observations

--- bff/src/test/java/bg/com/bo/bff/providers/dtos/response/certificates/CertificationPriceMWResponseFixture.java
+++ bff/src/test/java/bg/com/bo/bff/providers/dtos/response/certificates/CertificationPriceMWResponseFixture.java
@@ -6,19 +6,6 @@ import java.util.Arrays;
 
 public class CertificationPriceMWResponseFixture {
 
-//    public static CertificationPriceMWResponse withDefaults(){
-//        return CertificationPriceMWResponse.builder()
-//                .chargeFeeId(1)
-//                .eventId(1)
-//                .amount(123)
-//                .currencyDes("Bs")
-//                .currencyCode(0)
-//                .rangeFrom(1)
-//                .rangeTo(2)
-//                .rangeType("string")
-//                .build();
-//    }
-
     public static CertificationPriceMWResponse withDefaults(){
         return CertificationPriceMWResponse.builder()
                 .data(
