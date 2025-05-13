0a1ca9860a824e149c8b95abc714710283bcdfff
nicolas.andrade <jose.andrade@bg.com.bo>
2025-05-13 10:12:15 -0400

    cambio de formato en la fecha pÃara la respuesta del historico de certificados

--- bff/src/test/java/bg/com/bo/bff/application/dtos/response/certifications/CertificationResponseFixture.java
+++ bff/src/test/java/bg/com/bo/bff/application/dtos/response/certifications/CertificationResponseFixture.java
@@ -68,9 +68,7 @@ public class CertificationResponseFixture {
         return Arrays.asList(
                 CertificationHistoryResponse.builder()
                         .requestNumber("1268870")
-                        .day("21")
-                        .month("Ene")
-                        .year("2022")
+
                         .title("Certificado Cuentas")
                         .docNumber("CCU1268870-SC2022")
                         .state("Entregado")
@@ -78,9 +76,7 @@ public class CertificationResponseFixture {
                         .build(),
                 CertificationHistoryResponse.builder()
                         .requestNumber("1268870")
-                        .day("21")
-                        .month("Ene")
-                        .year("2022")
+                        .date("21/02/2021")
                         .title("Certificado Cuentas")
                         .docNumber("CCU1268870-SC2022")
                         .state("Entregado")
