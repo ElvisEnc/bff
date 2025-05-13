f7243f70b1109ec4234427189faf98d8d36acb66
nicolas.andrade <jose.andrade@bg.com.bo>
2025-05-13 11:41:44 -0400

    ajuste de validacions en request para pago de servicio

--- bff/src/main/java/bg/com/bo/bff/application/dtos/request/payment/service/PaymentDebtsRequest.java
+++ bff/src/main/java/bg/com/bo/bff/application/dtos/request/payment/service/PaymentDebtsRequest.java
@@ -26,6 +26,7 @@ public record PaymentDebtsRequest(
         Long fromAccountId,
 
         @NotBlank
+        @NotNull
         @Size(min = 1, max = 100)
         @Schema(description = "uuid", example = "324a029a-553f-4acb-abf4-4dcb25574463")
         String idGenerated,
@@ -55,10 +56,12 @@ public record PaymentDebtsRequest(
         String company,
 
         @OnlyNumber
+        @NotNull
         @Schema(description = "Código de servicio", example = "77")
         String serviceCode,
 
         @NotBlank
+        @NotNull
         @ValidText
         @Size(max = 150)
         @Schema(description = "Descripción", example = "Pagos")
